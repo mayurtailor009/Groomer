d7af5175b80a36d78ba260b19899476083b0274d
deepaksingh50815 <ds.singh50815@gmail.com>
2016-04-01 17:32:01 +0530

    arabic changes

--- app/src/main/java/com/groomer/shareexperience/ShareExperienceActivity.java
+++ app/src/main/java/com/groomer/shareexperience/ShareExperienceActivity.java
@@ -1,6 +1,7 @@
 package com.groomer.shareexperience;
 
 import android.os.Bundle;
+import android.view.View;
 
 import com.groomer.R;
 import com.groomer.activity.BaseActivity;
@@ -14,5 +15,20 @@ public class ShareExperienceActivity extends BaseActivity {
 
         setHeader(getString(R.string.share_title));
         setLeftClick(R.drawable.back_btn);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch(view.getId()) {
+            case R.id.hamburgur_img_icon:
+                onBackPressed();
+                break;
+        }
+    }
+
+
+    @Override
+    public void onBackPressed() {
+        this.finish();
     }
 }
