fcb384b59964a187db70348ddf463539ba8354a6
Mayur <mayur.tailor009@gmail.com>
2016-04-03 09:19:30 +0530

    change theme and size of km range

--- app/src/main/java/com/groomer/shareexperience/ShareExperienceActivity.java
+++ app/src/main/java/com/groomer/shareexperience/ShareExperienceActivity.java
@@ -11,6 +11,7 @@ public class ShareExperienceActivity extends BaseActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setTheme();
         setContentView(R.layout.activity_share_experience);
 
         setHeader(getString(R.string.share_title));
